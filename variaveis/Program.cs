f951c5df2e2cfc9d834481521926e91e73c111ed
AlunoIgor <alunoigor2024@yahoo.com>
2024-11-01 16:07:46 -0400

    tipos de dados primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// tipos de dados primitivos
+// Numeros inteiros
+short ano = 2025;
+int contador = 10;
+long populacaoMundial = 7800000000;
+
+// Numeros de ponto flutuante
+float temperatura = 43.6f;
+double precoProduto = 199.90;
+
+// Caracter
+char inicial = "W";
+
+// Tipos logicos
+bool estalogado = true;
+bool ativo = false;
+
