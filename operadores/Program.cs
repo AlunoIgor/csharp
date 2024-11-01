e839b823ee32002634fa94a9cb4b21ca9925aac4
AlunoIgor <alunoigor2024@yahoo.com>
2024-11-01 17:19:09 -0400

    Operados Aritmeticos 2

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Operadores Aritmeticos
-double resultado = 10 + 8 + 12 / 3;
-console.WriteLine(resultado);
+double resultado1= 10 + 8 + 12 / 3;
+console.WriteLine(resultado1);
 
-double resultado = (10 + 8 + 12) / 3;
-console.WriteLine(resultado);
+double resultado2= (10 + 8 + 12) / 3;
+console.WriteLine(resultado2);
 
-double resultado = (2 + 8) * 2 / 4;
-console.WriteLine(resultado);
+double resultado3= (2 + 8) * 2 / 4;
+console.WriteLine(resultado3);
