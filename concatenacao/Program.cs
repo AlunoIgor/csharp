40ac7cdc163ea6b580bbf1942fe917c30ceac66e
leonardorosa711 <leo00vilelarosa@gmail.com>
2024-11-04 17:15:01 -0400

    palavra chave typeof

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -4,11 +4,20 @@ Console.WriteLine("Hello, World!");
 // concatenacao
 
 string nome = "joao";
-string saudacao = "ola," + nome + "! Bem vindo.";
+string saudacao = "ola" + nome + "! Bem vindo.";
 Console.WriteLine(saudacao);
 
 // Interpolacao de strings ($)
 
-string nome = "joao"
-string saudacao1 = $"Ola, {nome}! Bem vindo,";
+string nome1 = "joao";
+string saudacao1 = $"Ola, {nome}! Bem vindo.";
 Console.WriteLine(saudacao);
+
+//Gettype
+
+int numero = 5;
+Console.WriteLine(numero.GetType());
+
+// palavra chave typeof
+
+Console.WriteLine(typeof(int));
